1af29831b7d3f65d9c9b9bfe1b3e80e78fcaf6b0
Marcos Allan <allanmenezes880@gmail.com>
2025-03-07 18:42:33 -0300

    estilizi a url de compartilhamento do site

--- src/components/GoogleLogin/index.jsx
+++ src/components/GoogleLogin/index.jsx
@@ -1,18 +1,91 @@
 //IMPORTAÇÃO DAS BIBLIOTECAS
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router'
 
 //IMPORTAÇÃO DOS ÍCONES
 import { FcGoogle } from "react-icons/fc";
 
+//IMPORTAÇÃO DOS SERVIÇOS DO FIREBASE
+import { auth, provider, signInWithPopup } from '../../utils/firebase.tsx'
+
+//IMPORTAÇÃO DO PROVEDOR DOS ESTADOS GLOBAIS
+import { GlobalContext } from "../../provider/context";
+
 export default function GoogleLogin() {
+
+    //UTILIZAÇÃO DO HOOK useNavigate
+    const navigate = useNavigate()
 
     //UTILIZAÇÃO DO HOOK useState
     const [isHover, setIsHover] = useState(false)
+
+    //IMPORTAÇÃO DAS VARIAVEIS DE ESTADO GLOBAL
+    const { theme, toggleLoading, toggleUser } = useContext(GlobalContext);
+
+    //FUNÇÃO RESPONSÁVEL POR FAZER LOGIN
+    function signIn(email, name) {
+        axios.post('https://backendsaferun.onrender.com/register-google', {
+            email: email,
+            name: name,
+        })
+        .then(function (response) {
+            
+            if(typeof response.data === 'object'){
+                toast.dismiss();
+                //CHAMA O MODAL DE SUCESSO
+                toast.success(`${response.data.message} ${response.data.person.name}`, {
+                    style: {
+                        backgroundColor: theme == 'light' ? "#FEFEFE" : "#0D0D0D",
+                        color: theme == 'light' ? "#0D0D0D" : "#FEFEFE"
+                    }
+                })
+
+                //COLOCA OS DADOS DO BACKEND DO USUÁRIO NO FRONTEND
+                toggleUser(response.data.person._id, response.data.person.name, response.data.person.email, response.data.person.historico_pedido, response.data.person.cart, response.data.person.client_type, true)
+
+                //NAVEGA PARA A PRÓXIMA PÁGINA
+                navigate('/principal')
+            }else{
+                toast.dismiss();
+                //CHAMA O MODAL DE ERRO
+                toast.error(response.data)
+            }
+        })
+        .catch(function (error) {
+            //ESCREVE O ERRO OCORRIDO NO CONSOLE
+            console.log(error)
+        })
+    }
+
+    //FUNÇÃO REPOSNSÁVEL POR FAZER LOGIN COM O GOOGLE
+    const handleGoogleLogin = async () => {
+        //MUDA O ESTADO DE CARREGAMENTO DA PÁGINA PARA true
+        toggleLoading(true)
+
+        try {
+            //REALIZA O LOGIN VIA POPUP DO GOOGLE
+            const result = await signInWithPopup(auth, provider);
+            
+            // FAZ LOGIN COM A CONTA DO USÁRIO
+            if(result.user){
+                //MUDA O ESTADO DE CARREGAMENTO DA PÁGINA PARA false
+                toggleLoading(false)
+                signIn(result.user.email || "", result.user.displayName || "")
+                console.log(result.user)
+            }
+        } catch (error) {
+            //ESCREVE O ERRO NO CONSOLE
+            console.error("Erro ao fazer login:", error);
+        }
+    };
 
     return(
         <div
             onMouseEnter={() => setIsHover(true)}
             onMouseLeave={() => setIsHover(false)}
+            onClick={() => handleGoogleLogin()}
             className={`w-[80%] flex items-center justify-center border-[1px] border-my-gray px-4 py-3 rounded-[6px] relative max-w-[700px] cursor-pointer`}
         >
             <FcGoogle className={`text-[32px] ${isHover == true ? 'rotate-[360deg]' : 'rotate-[0deg]'} transition-all duration-[.4s]`} />
--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -4,13 +4,12 @@ import { getAuth, GoogleAuthProvider, getRedirectResult, signInWithRedirect, sig
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxTGRt9-i0eB6o8uqivhxTBToiGtUKxgs",
-  authDomain: "presentebd.firebaseapp.com",
-  projectId: "presentebd",
-  storageBucket: "presentebd.appspot.com",
-  messagingSenderId: "567755857350",
-  appId: "1:567755857350:web:c648f003fcfb377de4e6c7",
-  measurementId: "G-WFFTBDDJZS"
+  apiKey: "AIzaSyA9PiLPPkKs6lnq2M5LjsxKe8l2gggI1bo",
+  authDomain: "saferun-8b3f2.firebaseapp.com",
+  projectId: "saferun-8b3f2",
+  storageBucket: "saferun-8b3f2.firebasestorage.app",
+  messagingSenderId: "886254534533",
+  appId: "1:886254534533:web:5f16b329445b3b65900eec"
 };
 
 //INICIA A APLICAÇÃO DO FIREBASE
